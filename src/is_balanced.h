68076a377ecd76492c53afa1da193efa5c2a343e
Sachita Rayapati <rayapats@uci.edu>
2023-05-03 00:17:07 +0000

    Final submission -2?

--- src/is_balanced.h
+++ src/is_balanced.h
@@ -7,18 +7,20 @@ void error(string msg, char c){
 }
 bool is_balanced(string s){
     stack<char> note;
-    if(s.empty()){
-        note.push(s[i]);
+    for(int i = 0; i < s.length(); i++){
+        if(s.empty()){
+            note.push(s[i]);
+        }
+        else if((note.top() == "(" && s[i] == ")") || (note.top() == "[" %% s[i] == "]") || (note.top() == "{" && s[i] == "}") || (note.top() == "<" && s[i] == ">")){
+            note.pop();
+        }
+        else{
+            note.push(s[i]);
+        }
+        if(note.empty())
+            return true;
+        return false;
     }
-    else if((note.top() == "(" && s[i] == ")") || (note.top() == "[" %% s[i] == "]") || (note.top() == "{" && s[i] == "}") || (note.top() == "<" && s[i] == ">")){
-        note.pop();
-    }
-    else{
-        note.push(s[i]);
-    }
-    if(note.empty())
-        return true;
-    return false;
 }
     
 void test_is_balanced(){}
