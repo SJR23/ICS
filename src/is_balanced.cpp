badfdabdb70091528cc76d78e381ab66913f38ce
Sachita Rayapati <rayapats@uci.edu>
2023-05-03 01:30:59 +0000

    Submitting final -1

---
+++ src/is_balanced.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+#include "is_balanced.h"
+
+void error(string msg, char c){
+    cout << "ERROR: " << msg << " " << c << endl;
+}
+bool is_balanced(string s){
+    stack<char> note;
+    for(int i = 0; i < s.length(); i++){
+        if(note.empty()){
+            note.push(s[i]);
+        }
+        else if ((note.top() == '(' && s[i] == ')') || 
+                (note.top() == '[' && s[i] == ']') || 
+                (note.top() == '{' && s[i] == '}') || 
+                (note.top() == '<' && s[i] == '>')){
+            note.pop();
+        }
+        else{
+            note.push(s[i]);
+        }
+        if(note.empty()){
+            return false;
+        }
+        return true;
+    }
+}
+    
+void test_is_balanced(){
+    string test_cases[] = {"({(())})((([({})])))(((((<>([{()}])(<>))))))()",
+                           "({(<>)})((([({})])))(((((()([{()}])(())))))", 
+                           "({(<>)})((([({})])))((((([]([{<>}])(()))))))()])"};
+    for(int val = 0; val <=2; val ++){
+        bool check = is_balanced(test_cases[val]);
+        cout << check << endl;
+    }
+}
--- src/is_balanced.h
+++ src/is_balanced.h
@@ -2,36 +2,7 @@
 #include <stack>
 using namespace std;
 
-void error(string msg, char c){
-    cout << "ERROR: " << msg << " " << c << endl;
-}
-bool is_balanced(string s){
-    stack<char> note;
-    for(int i = 0; i < s.length(); i++){
-        if(note.empty()){
-            note.push(s[i]);
-        }
-        else if ((note.top() == '(' && s[i] == ')') || 
-                (note.top() == '[' && s[i] == ']') || 
-                (note.top() == '{' && s[i] == '}') || 
-                (note.top() == '<' && s[i] == '>')){
-            note.pop();
-        }
-        else{
-            note.push(s[i]);
-        }
-        if(note.empty()){
-            return false;
-        }
-        return true;
-    }
-}
+void error(string msg, char c);
+bool is_balanced(string s);
     
-void test_is_balanced(){
-    string test_cases[] = [ "({(())})((([({})])))(((((<>([{()}])(<>))))))()",
-                           "({(<>)})((([({})])))(((((()([{()}])(())))))", 
-                           "({(<>)})((([({})])))((((([]([{<>}])(()))))))()])"];
-    for(val = 0; test_cases.length(); val ++){
-        bool check = is_balanced(test_cases[val];
-        cout << check << endl;
-    }
+void test_is_balanced();
