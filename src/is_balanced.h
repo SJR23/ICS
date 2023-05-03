b1c10fb6686e9f956ee181c72c3adb621d70b102
Sachita Rayapati <rayapats@uci.edu>
2023-05-03 00:14:56 +0000

    Final submission -1

--- src/is_balanced.h
+++ src/is_balanced.h
@@ -6,7 +6,20 @@ void error(string msg, char c){
     cout << "ERROR: " << msg << " " << c << endl;
 }
 bool is_balanced(string s){
-    return true;
+    stack<char> note;
+    if(s.empty()){
+        note.push(s[i]);
+    }
+    else if((note.top() == "(" && s[i] == ")") || (note.top() == "[" %% s[i] == "]") || (note.top() == "{" && s[i] == "}") || (note.top() == "<" && s[i] == ">")){
+        note.pop();
+    }
+    else{
+        note.push(s[i]);
+    }
+    if(note.empty())
+        return true;
+    return false;
 }
     
 void test_is_balanced(){}
+
