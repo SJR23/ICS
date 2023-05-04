7b61c812d0502dad0e71d6c14a17400fc963eace
Sachita Rayapati <rayapats@uci.edu>
2023-05-04 23:15:33 +0000

    Checking

--- src/is_balanced.cpp
+++ src/is_balanced.cpp
@@ -6,13 +6,34 @@ using namespace std;
 void error(string msg, char c){
     cerr << "ERROR: " << msg << " " << c << endl;
 }
-bool is_balanced(string s){
+
+/*bool is_balanced(string s){
     stack<char> note;
     for(int i = 0; i < s.length(); i++){
         if(note.size() == 0){
             note.push(s[i]);
         }
-        else if ((note.top() == '(' && s[i] == ')') || 
+        else if((note.top() == '(' && s[i] == ')') || 
+                (note.top() == '[' && s[i] == ']') || 
+                (note.top() == '{' && s[i] == '}') || 
+                (note.top() == '<' && s[i] == '>')){
+            note.pop();
+        }
+        else{
+            note.push(s[i]);
+        }
+    }
+    if(note.size() == 0){
+        return true;
+    }
+    return false;
+}*/
+
+bool is_balanced(string s){
+    stack<char> note;
+    for(int i = 0; i < s.length(); i++){
+        if(note.size()!=0){
+            if((note.top() == '(' && s[i] == ')') || 
                 (note.top() == '[' && s[i] == ']') || 
                 (note.top() == '{' && s[i] == '}') || 
                 (note.top() == '<' && s[i] == '>')){
@@ -32,7 +53,7 @@ void test_is_balanced(){
     string test_cases[] = {"({(())})((([({})])))(((((<>([{()}])(<>))))))()",
                            "({(<>)})((([({})])))(((((()([{()}])(())))))", 
                            "({(<>)})((([({})])))((((([]([{<>}])(()))))))()])"};
-    for(int val = 0; val <=2; val ++){
+    for(int val = 0; val <=2; val++){
         bool check = is_balanced(test_cases[val]);
         cout << check << endl;
     }
