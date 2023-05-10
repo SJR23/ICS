d7105474eea83f84335ba30e9bd65f43155fbcdc
Sachita Rayapati <rayapats@uci.edu>
2023-05-10 14:25:20 -0700

    8th submission try

--- src/stats.cpp
+++ src/stats.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 Stats::Stats(string name, const vector<int> & chain_lengths) : name(name){
-    //entries = chain_lengths.size();
-    //chains = chain_lengths.size();
+    entries = chain_lengths.size();
+    chains = chain_lengths.size();
     load_factor = (double)entries/chains;
     if(entries > 0){
         min = compute_min(chain_lengths);
         max = compute_max(chain_lengths);
-        //span = compute_span(max, min);
+        span = 0;
         mean = compute_mean(chain_lengths);
         stddev = compute_stddev(chain_lengths);
         for(int i : chain_lengths){
