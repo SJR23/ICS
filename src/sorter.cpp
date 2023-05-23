8fffff1036513be969ece37dbe9c5e5c51142abd
Sachita Rayapati <rayapats@uci.edu>
2023-05-23 08:34:01 +0000

    Trial 20

--- src/sorter.cpp
+++ src/sorter.cpp
@@ -43,14 +43,17 @@ bool Sorter::verify_sorted(){
 }
 
 void InsertionSorter::insertionsort(vector<string> & vec, int low, int high){
-    for(int i = low + 1; i <= high; i++){
-        string note = vec[i];
+    int j;
+    string note;
+    for(int i = 0; i < high; i++){
+        j = i;
         int j = i - 1;
-        while(j >= low && vec[j] > note){
-            vec[j+1] = vec[j];
-            j = j - 1;
+        while(j > low && vec[j] < vec[j-1]){
+            note = vec[j];
+            vec[j] = vec[j-1];
+            vec[j-1] = note;
+            j--;
         }
-        vec[j+1] = note;
     }
 }
 
