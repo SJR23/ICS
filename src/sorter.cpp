3352f1ab8f9afd4ab630cabb8defaa4fb183cd1e
Sachita Rayapati <rayapats@uci.edu>
2023-05-23 11:34:57 +0000

    Trial 23

--- src/sorter.cpp
+++ src/sorter.cpp
@@ -108,7 +108,7 @@ void QuickSorter::quicksort(vector<string> & vec, int low, int high){
 
 
 void QuickSorter::sort(){
-    quicksort(vec, 0, vec.size()-1);
+    quicksort(vec, 0, vec.size());
 }
 
 void HeapSorter::heapify(vector<string> & vec, int high, int root){
