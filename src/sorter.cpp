9168f22bea7b8071f50f1862a560c6e12bb512e8
Sachita Rayapati <rayapats@uci.edu>
2023-05-24 05:21:52 +0000

    Trial 30

--- src/sorter.cpp
+++ src/sorter.cpp
@@ -117,17 +117,17 @@ void HeapSorter::heapify(vector<string> & vec, int high, int root){
 
 void HeapSorter::heapsort(vector<string> & vec, int low, int high){
     int size = high;
-    for(int root = size/2-1; root >= 0; --root){
+    for(int root = size/2-1; root >= 0; root--){
         heapify(vec, size, root);
     }
-    for(int end = size-1; end>0; --end){
+    for(int end = size; end>=0; end--){
         swap(vec[end], vec[low]);
         heapify(vec, end, low);
     }
 }
 
 void HeapSorter::sort(){
-    heapsort(vec, 0, vec.size());
+    heapsort(vec, 0, vec.size()-1);
 }
 
 void introsort_util(vector<string>& arr, int low, int high, int depth_limit) {
