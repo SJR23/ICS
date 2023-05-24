0e0e70b30e81b10b1758fceb5580035ef930eda8
Sachita Rayapati <rayapats@uci.edu>
2023-05-24 05:59:07 +0000

    Trial a lot

--- src/sorter.cpp
+++ src/sorter.cpp
@@ -117,10 +117,10 @@ void HeapSorter::heapify(vector<string> & vec, int high, int root){
 
 void HeapSorter::heapsort(vector<string> & vec, int low, int high){
     for(int root = high/2-1; root >= 0; root--){
-        heapify(vec, size, root);
+        heapify(vec, high, root);
     }
     for(int end = high-1; end>=0; end--){
-        swap(vec[low], vec[end]);
+        swap(vec[0], vec[end]);
         heapify(vec, end, 0);
     }
 }
