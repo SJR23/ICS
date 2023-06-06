6ff9ce20597e8e61d7c14f586093b2d2234e410b
Sachita Rayapati <rayapats@uci.edu>
2023-06-06 15:43:55 -0700

    Trial 20

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -46,7 +46,7 @@ bool is_adjacent(const string & word1, const string & word2){
     for(int i = 0; i < small; i++){
         if(word1[i] != word2[i]){
             c++;
-            if(c>1)return false;
+            //if(c>1)return false;
         }
     }
     if(len1!=len2) c=1;
