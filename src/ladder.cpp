e0305b3e2e289cc6fd70f14e7a6fb9d4a5ca77e8
Sachita Rayapati <rayapats@uci.edu>
2023-06-06 23:49:52 -0700

    please work pt.2

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -35,7 +35,39 @@ bool edit_distance_within(const std::string & str1, const std::string & str2, in
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
-    return edit_distance_within(word1, word2, 1);
+    int len1 = word1.length();
+    int len2 = word2.length();
+
+    if(abs(len1-len2) > 1)
+        return false;
+
+    if(len1 == len2){
+        int note = 0;
+        for(int i = 0; i < len1; i++){
+            if(word1[i] != word2[i]){
+                note++;
+                if(note > 1)
+                    return false;
+            }
+        }
+        return note == 1;
+    }
+    int i = 0, j = 0, note = 0;
+    while (i < len1 && j < len2){
+        if (word1[i] != word2[j]){
+            note++;
+            if(note > 1)
+                return false;
+            if(len1 < len2)
+                j++;
+            else
+                i++;
+        }
+        else{
+            i++; j++;
+        }
+    }
+    return note == 1;
 }
 
 void load_words(set<string> & word_list, const string & file_name){
